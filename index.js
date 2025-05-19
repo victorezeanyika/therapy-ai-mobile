1b1622597d5b5136fe0e0dd0643c90d0d94680c1
samuel <adeyemis710@gmail.com>
2025-05-19 09:24:56 +0100

    fixes for build

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
